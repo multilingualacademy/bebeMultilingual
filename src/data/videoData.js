3e0d971696534348b11ed1439fffb26e8793ccd0
multilingualacademy <multilingualacademyspanish@gmail.com>
2025-03-02 12:19:47 -0500

    first commit

---
+++ src/data/videoData.js
@@ -0,0 +1,232 @@
+export const videoData = [
+    {
+      id: 1,
+      title: "DVD 1 Español - Inglés",
+      primaryFlagCode: "ES", // Bandera de España
+      secondaryFlagCode: "GB", // Bandera del Reino Unido
+      videos: [
+        "https://www.youtube.com/embed/W46-VHmrIas",
+        "https://www.youtube.com/embed/pcEMHNh2nkg",
+        "https://www.youtube.com/embed/84rRYStTO7M",
+        "https://www.youtube.com/embed/ER7e4QqZIO8",
+        "https://www.youtube.com/embed/kCFKudIeCdw",
+        "https://www.youtube.com/embed/YyOMDTKgNG4",
+        "https://www.youtube.com/embed/TgVd59-xvHA",
+        "https://www.youtube.com/embed/MyHabN3Qdho",
+        "https://www.youtube.com/embed/rwyPuZ-TQLY",
+        "https://www.youtube.com/embed/0Ef0kxyY0L8",
+        "https://www.youtube.com/embed/oIouxt_dWVM",
+        "https://www.youtube.com/embed/k9Ly0OXk2PE",
+        "https://www.youtube.com/embed/65Y56ulWOCA",
+        "https://www.youtube.com/embed/cgsnsQnFfRQ",
+        "https://www.youtube.com/embed/EDCDlv-I2as",
+      ],
+    },
+    {
+      id: 2,
+      title: "DVD 2 Inglés - Español",
+      primaryFlagCode: "GB", // Bandera de España
+      secondaryFlagCode: "ES", // Bandera del Reino Unido
+      videos: [
+        "https://www.youtube.com/embed/k_85Tc26e8Y",
+        "https://www.youtube.com/embed/0wBFJYXhWJg",
+        "https://www.youtube.com/embed/6DczT4rZ-08",
+        "https://www.youtube.com/embed/hYNzRBAIJAg",
+        "https://www.youtube.com/embed/fDAmI_o_7u8",
+        "https://www.youtube.com/embed/dbqvLmY7LCg",
+        "https://www.youtube.com/embed/FQ2oUxhidvk",
+        "https://www.youtube.com/embed/qQUhS4UHRIU",
+        "https://www.youtube.com/embed/t6TDKrBFZM0",
+        "https://www.youtube.com/embed/HfONfKsuEqo",
+        "https://www.youtube.com/embed/JzYcNrkvzdc",
+        "https://www.youtube.com/embed/EJZ4yxgazjM",
+        "https://www.youtube.com/embed/P1mly5Ift_g",
+        "https://www.youtube.com/embed/EVgaPQzHIqI",
+        "https://www.youtube.com/embed/XH3Wh94HnjE",
+      ],
+    },
+    {
+      id: 3,
+      title: "DVD 3 Italiano - Alemán",
+      primaryFlagCode: "IT", // Bandera de España
+      secondaryFlagCode: "DE", // Bandera del Reino Unido
+      videos: [
+        "https://www.youtube.com/embed/DobFpZa1MXw",
+        "https://www.youtube.com/embed/k4rmaTLmpic",
+        "https://www.youtube.com/embed/I0IBbFQeA8g",
+        "https://www.youtube.com/embed/YzLjTHZz3B0",
+        "https://www.youtube.com/embed/fZ3FFVi3Z8o",
+        "https://www.youtube.com/embed/TnByRQhPHfM",
+        "https://www.youtube.com/embed/Baxjqi3ewvA",
+        "https://www.youtube.com/embed/oFMqBRYyLY8",
+        "https://www.youtube.com/embed/YJZWW91SewY",
+        "https://www.youtube.com/embed/Enk3jxdSYHA",
+        "https://www.youtube.com/embed/9oMs94VxaGI",
+        "https://www.youtube.com/embed/alE_ykAClc8",
+        "https://www.youtube.com/embed/j_IImZo1AJc",
+        "https://www.youtube.com/embed/h_zWZ8b2Ja0",
+        "https://www.youtube.com/embed/nfQLbLKTIQU"
+      ],
+    },
+    {
+      id: 4,
+      title: "DVD 4 Alemán - Italiano",
+      primaryFlagCode: "DE", // Bandera de España
+      secondaryFlagCode: "TI", // Bandera del Reino Unido
+      videos: [
+        "https://www.youtube.com/embed/kV73mEL8-EM",
+        "https://www.youtube.com/embed/T2NWSSNk7Bw",
+        "https://www.youtube.com/embed/J1HFy0UBI6M",
+        "https://www.youtube.com/embed/GfQ0cIai-bk",
+        "https://www.youtube.com/embed/ntUvWN6H6Y0",
+        "https://www.youtube.com/embed/HEs2zkDYKRk",
+        "https://www.youtube.com/embed/7RRHZJRBH2o",
+        "https://www.youtube.com/embed/q5yOPvqJiCs",
+        "https://www.youtube.com/embed/E2mesjGAYsk",
+        "https://www.youtube.com/embed/zZaaY0pbHUk",
+        "https://www.youtube.com/embed/UHSyeOFHeaU",
+        "https://www.youtube.com/embed/KKUb2DWDZa0",
+        "https://www.youtube.com/embed/Gh4PnNELGsY",
+        "https://www.youtube.com/embed/aXxTnAQLwX0",
+        "https://www.youtube.com/embed/sNuu1IuPOc0"
+      ],
+    },
+    {
+      id: 5,
+      title: "DVD 5 Francés - Portugués",
+      primaryFlagCode: "FR", // Bandera de España
+      secondaryFlagCode: "BR", // Bandera del Reino Unido
+      videos: [
+        "https://www.youtube.com/embed/ta6yHEMZsRM",
+        "https://www.youtube.com/embed/WaPSdM7BJdQ",
+        "https://www.youtube.com/embed/uKEKiaiMs30",
+        "https://www.youtube.com/embed/Xfrt1QiehpE",
+        "https://www.youtube.com/embed/aBmGqUVa2M4",
+        "https://www.youtube.com/embed/blbFK8MXRj4",
+        "https://www.youtube.com/embed/twrF97xWRpI",
+        "https://www.youtube.com/embed/Adxn7xtBiOg",
+        "https://www.youtube.com/embed/QC_r2muFkWg",
+        "https://www.youtube.com/embed/VHYGdeCOh14",
+        "https://www.youtube.com/embed/vB_OxHyEpio",
+        "https://www.youtube.com/embed/Vj_8EqJgMzY",
+        "https://www.youtube.com/embed/yxwYpIZj5MI",
+        "https://www.youtube.com/embed/njAiJGtZvLM",
+        "https://www.youtube.com/embed/iQL4z_aI2ic"
+      ],
+    },
+    {
+      id: 6,
+      title: "DVD 6 Portugués - Francés",
+      primaryFlagCode: "BR", // Bandera de España
+      secondaryFlagCode: "FR", // Bandera del Reino Unido
+      videos: [
+        "https://www.youtube.com/embed/L3LrIAR1umM",
+        "https://www.youtube.com/embed/-L89NmXLlaY",
+        "https://www.youtube.com/embed/Ia29opjQezc",
+        "https://www.youtube.com/embed/WUj-IBRNDdM",
+        "https://www.youtube.com/embed/Gi2wMY422iM",
+        "https://www.youtube.com/embed/2ZJJYW5I4vI",
+        "https://www.youtube.com/embed/lswjj_TpFxY",
+        "https://www.youtube.com/embed/F6Wah-yy7tc",
+        "https://www.youtube.com/embed/ZOYk5qfQ42Q",
+        "https://www.youtube.com/embed/lxw6GxDvG4Y",
+        "https://www.youtube.com/embed/8R_U8279rIQ",
+        "https://www.youtube.com/embed/4EjtCowcMLc",
+        "https://www.youtube.com/embed/eKdFLx06jho",
+        "https://www.youtube.com/embed/Z3usJaN59m0",
+        "https://www.youtube.com/embed/cNdhiGvJSrM"
+      ],
+    },
+    {
+      id: 7,
+      title: "DVD 7 Mandarín - Todos",
+      primaryFlagCode: "CN", // Bandera de España
+      secondaryFlagCode: [], // Bandera del Reino Unido
+      videos: [
+        "https://www.youtube.com/embed/PeT8fgTpYM4",
+        "https://www.youtube.com/embed/ZfF8rBIzrv8",
+        "https://www.youtube.com/embed/7tk-ldBHCAQ",
+        "https://www.youtube.com/embed/ECW0ZesxyQk",
+        "https://www.youtube.com/embed/yuM83p0s7Fw",
+        "https://www.youtube.com/embed/-w1vCg95XCw",
+        "https://www.youtube.com/embed/_JcXPAH8DAw",
+        "https://www.youtube.com/embed/V9e2kqTSd1U",
+        "https://www.youtube.com/embed/zpdMro6Tug8",
+        "https://www.youtube.com/embed/HvNleNENCV8",
+        "https://www.youtube.com/embed/-HARL53CgEE",
+        "https://www.youtube.com/embed/2Uv4ffedo2Q",
+        "https://www.youtube.com/embed/RonIDfl58zU",
+        "https://www.youtube.com/embed/pJtwLKJ7xPA",
+        "https://www.youtube.com/embed/FlGCiboFMYg"
+      ]
+    },
+    {
+      id: 8,
+      title: "DVD 8 Mandarín - Todos",
+      primaryFlagCode: "CN", // Bandera de España
+      secondaryFlagCode: [],
+      videos: [
+        "https://www.youtube.com/embed/PWltCYc7fGc",
+        "https://www.youtube.com/embed/B5DELBFo3xk",
+        "https://www.youtube.com/embed/4AYjsIXjaRk",
+        "https://www.youtube.com/embed/hU_frdQWkrI",
+        "https://www.youtube.com/embed/y6H-ZJ3shJQ",
+        "https://www.youtube.com/embed/ekVmoSd5qPU",
+        "https://www.youtube.com/embed/4D_pqgIcsww",
+        "https://www.youtube.com/embed/c3wlQ0UBTh8",
+        "https://www.youtube.com/embed/6Vx8RbeFFr0",
+        "https://www.youtube.com/embed/D7M7tTtb_vo",
+        "https://www.youtube.com/embed/RIKkGrpgK4M",
+        "https://www.youtube.com/embed/ktjRbM64bKA",
+        "https://www.youtube.com/embed/bGMCSqUldnY",
+        "https://www.youtube.com/embed/bUuiV4zRl4g",
+        "https://www.youtube.com/embed/FdpUT0lTVUM"
+      ],
+    },
+    {
+      id: 9,
+      title: "DVD 9 Palabras - Todos",
+      primaryFlagCode: "CO",
+      videos: [
+        "https://www.youtube.com/embed/vAiAdxxnWQA",
+        "https://www.youtube.com/embed/HrEWlFg8QiU",
+        "https://www.youtube.com/embed/e9Bn5Di4778",
+        "https://www.youtube.com/embed/0jbilP9S0Oo",
+        "https://www.youtube.com/embed/gcGOZ4o_5wI",
+        "https://www.youtube.com/embed/-tAus-VxeZM",
+        "https://www.youtube.com/embed/JLly9F3h8HQ",
+        "https://www.youtube.com/embed/eLd5KvV7lG4",
+        "https://www.youtube.com/embed/SOyAGvuwxwk",
+        "https://www.youtube.com/embed/-OFtOspYnG0",
+        "https://www.youtube.com/embed/i9cERTeMuZw",
+        "https://www.youtube.com/embed/ColgH0a-0X8",
+        "https://www.youtube.com/embed/f6XAS5vPjYY",
+        "https://www.youtube.com/embed/Yq_JhcMm9WE",
+        "https://www.youtube.com/embed/Nld3-HO0QOw"
+      ],
+    },
+    {
+      id: 10,
+      title: "DVD 10 Palabras - Todos",
+      primaryFlagCode: "CO",
+      videos: [
+        "https://www.youtube.com/embed/5Ahf0YVdfRc",
+        "https://www.youtube.com/embed/ad6jvIJzx3k",
+        "https://www.youtube.com/embed/xuur1Mq04J8",
+        "https://www.youtube.com/embed/j5ktjn5jk5c",
+        "https://www.youtube.com/embed/DaD5RF3U-kc",
+        "https://www.youtube.com/embed/LA_TzgyGwmc",
+        "https://www.youtube.com/embed/IqyUcA0DGoE",
+        "https://www.youtube.com/embed/89Vnc0bpp78",
+        "https://www.youtube.com/embed/9-UOYzHkUKU",
+        "https://www.youtube.com/embed/W7JW0ItXfnU",
+        "https://www.youtube.com/embed/CaoYSb4ZTUA",
+        "https://www.youtube.com/embed/jQeiSWTXVVE",
+        "https://www.youtube.com/embed/KAP_4lbiy6I",
+        "https://www.youtube.com/embed/s0nl4tQMkZI",
+        "https://www.youtube.com/embed/wpaim4AkxrE",
+        "https://www.youtube.com/embed/pvlph2qMUG8",
+        "https://www.youtube.com/embed/HY3hnJRWs_8"
+      ],
+    },
+  ];
